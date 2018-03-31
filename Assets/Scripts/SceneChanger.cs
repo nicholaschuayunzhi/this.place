696c6b2eac0a2aee37948e05d6d7942ec729fc5a
nicholaschuayunzhi <nicholaschuayunzhi@gmail.com>
2018-03-31 22:23:52 +0800

    feat: add scene controller to change level

---
+++ Assets/Scripts/SceneChanger.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SceneChanger : MonoBehaviour
+{
+
+    public string Unload;
+    public string Load;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            SendMessageUpwards("Load", Load);
+        }
+
+        if (other.CompareTag("Player"))
+        {
+            SendMessageUpwards("Unload", Unload);
+        }
+    }
+}
---
+++ Assets/Scripts/SceneController.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneController : MonoBehaviour
+{
+
+    private void Awake()
+    {
+        Load("Tutorial");
+        Load("Player");
+    }
+
+    void Load(string sceneName)
+    {
+        if (!SceneManager.GetSceneByName(sceneName).isLoaded)
+        {
+            SceneManager.LoadSceneAsync(sceneName, LoadSceneMode.Additive);
+        }
+    }
+
+    void Unload(string sceneName)
+    {
+        if (SceneManager.GetSceneByName(sceneName).isLoaded)
+        {
+            SceneManager.UnloadSceneAsync(sceneName);
+        }
+    }
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
